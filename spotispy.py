55e7dd59e82dc9255605b4247525c0311cf0e281
JakubNowek <jakubbnowek@gmail.com>
2024-01-14 16:37:30 +0100

    added .config file for storing credentials

--- spotispy.py
+++ spotispy.py
@@ -1,8 +1,6 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-import pandas as pd
 import json
-from jsondiff import diff
 
 
 def get_followed_artist_list(sp, limit=50) -> list[dict]:
@@ -18,10 +16,10 @@ def get_followed_artist_list(sp, limit=50) -> list[dict]:
     return artist_list
 
 
-def get_artist_albums(sp, artist_id, album_type=('album', 'single'), limit=50) -> list[dict]:
+def get_artist_albums(sp, artist_id, album_types=('album', 'single'), limit=50) -> list[dict]:
     albums = []
-    for type in album_type:
-        results = sp.artist_albums(artist_id, album_type=type, limit=limit)
+    for album_type in album_types:
+        results = sp.artist_albums(artist_id, album_type=album_type, limit=limit)
         #print(results['total'])
         albums.extend(results['items'])
         while results['next']:
@@ -106,19 +104,7 @@ def read_data_from_json(filename: str, list_name: str):
     return list_from_main_json
 
 
-# def tracks_id_to_add_creator(sp, artists_id_list):
-#     list_of_tracks_id_to_add = []
-#     for artist_id in artists_id_list:
-#         list_of_tracks_id_to_add.extend(artist_tracks_to_add(sp, artist_id))
-#
-#     return list_of_tracks_id_to_add
-
-
-# ta funkcja jest w sumie na razie bez sensu bo ma tyle argumentów co wywołanie i tyle
-# def add_tracks_to_playlist(sp, tracks_id_list, playlist_id):
-#     sp.playlist_add_items(playlist_id=playlist_id, items=tracks_id_list)
-
-# setting the scopes to authorize
+""" Setting the scopes to authorize """
 scope = 'user-follow-read \
         user-library-read \
         user-top-read \
@@ -127,116 +113,63 @@ scope = 'user-follow-read \
         user-read-private \
         user-read-email'
 
-# client credentials
-cid = '69cb1a2b53aa409ebbf168afe0bd4b02'
-secret = '84a4ef954c3b4327af66350a76496d04'
-username = 'yt2fz80w9y0gxc49640wbxeub'
-artist_to_add_list = [['Chef Lay', '6xQoqRtTEYNZNF5gUMG3iu', 590],
-['CHOPPA TEE', '12iGnI7m6kxA8LpEQ1xdiJ', 91],
-['Ash Bash Tha Rapper', '4GHSsO2bndtYIfy8js9hUN', 370],
-['LeeLee Babii', '4sq5z4vRwCaR9tYr4lhvYj', 416],
-['Liddy Mechelle', '6wvAw2O489yb5baR7jDxKT', 588],
-['Aporshianta', '3hDxk4BmL68RMkEHGV4mBI', 327],
-['Stephanie Palomares', '1IvMHLX4QynHiuSVikYoP2', 113],
-['Jeane Marie', '3yP7Ekj2BVXARVv7nvcU6r', 350],
-['Mia lourde', '0vJ7ktn9hZt6T0vqBLNF1R', 80],
-['Stvr Baby', '2BxIP27i38uM4Ds5Pbdk3h', 191],
-['Sharondaduhdon', '38nZfbBg9YbejAt4Zzzy6Y', 270],
-['Leyla Blue', '6HpIVA13SPof8sYuXRUfxj', 538],
-['Bri Trilla', '0bUkjDAvq74Ppu5rSp1SN8', 52],
-['Mrz. Showbizz', '5Pj95JbfrrIQJMbJLKJsVk', 468],
-['Tinkaabellaaa', '6yB3T4NveoMCbzGupoPkvd', 591],
-['Five2blasian', '5Mnet43StJMpH4Jc12tdwa', 465],
-['Quality Bunny', '1JkHWrIYwyJVtwMtNObLze', 115],
-['Lala2muchhh', '7BXwzjTdFNOc6ZrxQk0at8', 610],
-['Mala', '4s2QCd13TEb6gTkPLIX9w4', 415],
-['Daisy', '290RgPy7mVNjeiCVY33yC3', 189],
-['Natasia', '3PsRFlJ0Lf94lNET6VGZSm', 306],
-['Dee Duchess', '45y9bdytJiHdKwu3Ug6Etj', 359],
-['Pussy Riot', '2hThsqaVEAWhWPBXnaOfB9', 231],
-['Auntie Piggy', '6hkSisAwJkzQ3UkCqjz976', 568],
-['Lavish Caminshion', '0TCsN3TDu9ZL5b54hQzz93', 35],
-['Kae Kajora', '4FZs29EztyuuQknNJ7oUT9', 369],
-['NajaMillion', '4xjFbnIaXePGFfYFe7xd1R', 424],
-['Big Indo', '6S6ITvCmnsccK8Sdv8vqL1', 551],
-['Princess Bre', '1V2oxBGVXw1JwZTvr03YVJ', 135],
-['Kayla B', '5yvTe64rt7c77FUaskqaCZ', 517],
-['Bbyafricka', '019gRg7DezPMbaI1xRZD6W', 1],
-['Nija', '7f9KxQWD88MZrSY6jc0zoW', 633],
-['ItsCourtney!', '0bZovW36bYttxuwptidwIL', 53],
-['Heavy Baile', '2E4sNJOOy9hae0J8DkT43M', 194],
-['chriseanrock', '3Idu5nTg2S3wrYwVkPqiwa', 291],
-['Reneé Rapp', '2hUYKu1x0UZQXvzCmggvSn', 232],
-['Stony', '1gfXNiyfLEKouLvNPmrvLk', 157],
-['BriBiase', '4tNro16CgwzfDfLK9YgFDl', 418],
-['TRINA', '7hLQp9nd2WwuSd9urqu3pJ', 639],
-['Reina Reign', '4b1OYOIVOk5MHhq9fJft8A', 394],
-['Shawna & Mia', '1IbE7R5bQCSBeMhVRbWnU7', 112],
-['Intuwiish', '0MH4kp4bzWFsx0OBWh5GwV', 25],
-['Supa Cindy', '22e8seQKsb34z8dW26bG5P', 184],
-['Nikki Jayy', '4MONW063BePUr0702vTw6c', 379],
-['Coco Bliss', '2oXb6vq3maLjFiFWLL5MpA', 245],
-['Notorious Bxtch', '0RF7Kx0y6KgNNAAdoGzRXJ', 31],
-['A. Chic', '09bs0SLPQ7Mjfsg2Rom6MO', 10],
-['Golden Ego', '5zBPlMAiHtUs5zLjkW3b6F', 518],
-['Front Paije', '5t0Y1ulwuavBxJqxI3CMrl', 510],
-['Misshy Money', '2SY1HckMjuERyoLAkb2x8D', 212],
-['Marlé Blu', '36Tyg8d0rbi8HTVyynvA7s', 266],
-['Lil Reeka', '2eZ44pc86sB6o1qU4xW4Qe', 226],
-['Big Boss Blossom', '6QvbxO1OIuWVxVS2SH9Rrg', 549],
-['Emony Keelen', '5zLaDcImyXoOM2DQMmEyoU', 520],
-['MONI DA G', '4twqXMj5X9jIGTAwrZLwYI', 420],
-['Peaches Adaba', '1AVlRnmtTppAMrm96iEfJu', 99],
-['Bunny Liiu', '6vEgaRuQU3vD4ae9SrL7tR', 585]]
+""" Client credentials """
 
-target_playlist_id = '6UDrcXzV1goOZldp8nOZum'
+
+""" Load credentials from .config file """
+credentials = read_data_from_json('.config', 'credentials')
+cid = credentials["cid"]
+secret = credentials["secret"]
+username = credentials["username"]
+
+
+target_playlist_id = '6UDrcXzV1goOZldp8nOZum'  # TestSpotispy
 auth_manager = SpotifyOAuth(username=username,
                             scope=scope,
                             client_id=cid,
                             client_secret=secret,
                             redirect_uri='http://localhost:3000')
 
-sp = spotipy.Spotify(auth_manager=auth_manager)
-
-
-followed_artist_list = get_followed_artist_list(sp)
-# for artist in followed_artist_list:
-#     print(artist['name'], artist['id'])
-
-""" Load old list of artists"""
-old_list = read_data_from_json('old_list_of_artists.json', 'artists')
-
-""" Generate new list of followed artists and transform it into json format"""
-new_dict_of_artists_json_to_export = export_spotify_dict_to_json_format(followed_artist_list, "artists",
-                                                                        [("artist_name", "name"), ("artist_id", "id")])
-new_list = new_dict_of_artists_json_to_export["artists"]
-
-""" Generate difference between new and old artists lists """
-new_added_artists_list = [x for x in new_list if x not in old_list]  # czyli wszystko co się pojawiło nowe
-artists_deleted_from_old_list = [x for x in old_list if x not in new_list]  # czyli wszystko co było usunięte z nowym
-
-print('What is new: ', new_added_artists_list)
-print('What has been deleted: ', artists_deleted_from_old_list)
-
-""" In case of any failure save new_added_artists_list and artists_deleted_from_old_list to json file """
-last_added_json_format = {"artists": new_added_artists_list}
-last_deleted_json_format = {"artists": artists_deleted_from_old_list}
-
-export_json_to_file(last_added_json_format, "last_added_artists.json")
-export_json_to_file(last_deleted_json_format, "last_deleted_artists.json")
-
-print("New artists count: ", len(new_added_artists_list))
-#TODO jak skoncze kombinowac to trzeba zmienic nazwe pliku na old zeby sie nadpisywalo
-
-""" Overwrite old list of artists with new_added_artists_list """
-export_json_to_file(new_dict_of_artists_json_to_export, "old_list_of_artists.json")
-
-""" Add all new songs to playlist artist by artist from new_added_artists_list """
-for artist in new_added_artists_list:
-    items = artist_tracks_to_add(sp, artist["artist_id"])
-    while items:
-        sp.playlist_add_items(playlist_id=target_playlist_id, items=items[:100], )
-        items = items[100:]
+# sp = spotipy.Spotify(auth_manager=auth_manager)
+#
+#
+# followed_artist_list = get_followed_artist_list(sp)
+# # for artist in followed_artist_list:
+# #     print(artist['name'], artist['id'])
+#
+# """ Load old list of artists"""
+# old_list = read_data_from_json('old_list_of_artists.json', 'artists')
+#
+# """ Generate new list of followed artists and transform it into json format"""
+# new_dict_of_artists_json_to_export = export_spotify_dict_to_json_format(followed_artist_list, "artists",
+#                                                                         [("artist_name", "name"), ("artist_id", "id")])
+# new_list = new_dict_of_artists_json_to_export["artists"]
+#
+# """ Generate difference between new and old artists lists """
+# new_added_artists_list = [x for x in new_list if x not in old_list]  # czyli wszystko co się pojawiło nowe
+# artists_deleted_from_old_list = [x for x in old_list if x not in new_list]  # czyli wszystko co było usunięte z nowym
+#
+# print('What is new: ', new_added_artists_list)
+# print('What has been deleted: ', artists_deleted_from_old_list)
+#
+# """ In case of any failure save new_added_artists_list and artists_deleted_from_old_list to json file """
+# last_added_json_format = {"artists": new_added_artists_list}
+# last_deleted_json_format = {"artists": artists_deleted_from_old_list}
+#
+# export_json_to_file(last_added_json_format, "last_added_artists.json")
+# export_json_to_file(last_deleted_json_format, "last_deleted_artists.json")
+#
+# print("New artists count: ", len(new_added_artists_list))
+#
+# """ Overwrite old list of artists with new_added_artists_list """
+# export_json_to_file(new_dict_of_artists_json_to_export, "old_list_of_artists.json")
+#
+# """ Add all new songs to playlist artist by artist from new_added_artists_list """
+# for artist in new_added_artists_list:
+#     items = artist_tracks_to_add(sp, artist["artist_id"])
+#     while items:
+#         sp.playlist_add_items(playlist_id=target_playlist_id, items=items[:100], )
+#         items = items[100:]
 
 # # print(sp.current_user())  # print userinfo
 # # sp.user_playlist_create(username, 'TestSpotispy', public=True, collaborative=False, description="Test playlist for SpotiSpy")
